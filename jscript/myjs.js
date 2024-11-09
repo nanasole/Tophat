decf77ef7650dddc25c85a1abaf26cab30b1752a
nanasole <tznana@yahoo.com>
2024-11-09 02:37:14 +0000

    added main js code

---
+++ jscript/myjs.js
@@ -0,0 +1,18 @@
+// window.addEventListener('load', (event) => {
+//     console.log('The page has fully loaded');
+//     });
+// document.body.innerHTML= "Hello";
+const el = document.getElementById("spanForJs")
+const tg = el.getElementsByTagName("span")
+// for (const element of tg) {
+//     element.innerHTML = "Testing "
+// };
+console.log(el)
+console.log(tg)
+
+const mem = document.querySelector("member")
+console.log(mem)
+const divs = document.querySelector("div")
+console.log(divs)
+const cl = document.querySelector("members")
+console.log(cl)
